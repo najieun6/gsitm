7fa58d25537715c921929d872c5f06d8c25d90a6
najieun <najiji71@gmail.com>
2024-07-05 11:33:02 +0900

    Initial commit

---
+++ src/test/java/Gsitm/Team2/Team2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Gsitm.Team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
